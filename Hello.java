36bbab50406d343a6117c6121fdabe166e623e17
Yan Liu <yanliu3@LABS.SCE.CARLETON.CA>
2017-10-02 13:02:10 -0400

    Your message

---
+++ Hello.java
@@ -0,0 +1,22 @@
+package lab4;
+
+
+
+public class Hello {
+
+		public static main(String[] args) 
+		{
+			Hello hello = new Hello();
+			hello.methodA();
+			hello.methodB();
+			hello.methodC();
+		}
+
+		public void methodA(void ) { }
+
+		public void methodb(void) { }
+
+		public void methodC(void) { }
+	}
+
+
